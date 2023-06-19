e32d49050d8b586ace455626e44c801b3e763e28
Gary Cen <cenjiajie@gmail.com>
2023-06-18 18:11:50 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import "@aws-amplify/ui-react/styles.css";
+import "@aws-amplify/ui-react/styles.css"; 
 import { API } from "aws-amplify";
 import {
   Button,
